de019e23f96f6bf179697447f6616c03338e596f
Annabelle Ineza <inezannabelle@gmail.com>
2023-12-01 17:33:27 +0200

    restart

--- python-network_0/100-status_code.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# sends request to URL passed as argument and displays status code
-curl -sI "$1" -o /dev/bull -w "%{http_code}"
--- python-network_0/101-post_json.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# sends data from a file and displays response
-curl -sX POST "$1" -d @"$2" -H "content-type: application/json"
