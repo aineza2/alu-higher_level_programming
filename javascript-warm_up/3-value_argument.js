d8364582c841c16175c6a2dd827dc0158d835a5c
aineza2 <a.ineza2@alustudent.com>
2024-02-02 05:32:05 -0800

    semistandard validation

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 const firstArg = process.argv[2];
-console.log(firstArg || "No argument");
+console.log(firstArg || 'No argument');
--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,13 +3,13 @@
 const firstArg = process.argv[2];
 
 if (isNaN(firstArg)) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   const n = Number(firstArg);
 
-  let line = "";
+  let line = '';
 
-  for (let i = 0; i < n; i++) line += "X";
+  for (let i = 0; i < n; i++) line += 'X';
 
   for (let i = 0; i < n; i++) console.log(line);
 }
